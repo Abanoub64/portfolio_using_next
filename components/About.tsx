84928d77dfcd03cd05a23f8a3382e33caee2472a
Abanoub <abanoubmilad64@gmail.com>
2025-01-16 05:02:45 +0200

    fixed about section

--- components/About.tsx
+++ components/About.tsx
@@ -19,9 +19,9 @@ function About() {
     >
       <Sectionheading>About me</Sectionheading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className=" font-bold">Engineering</span>, I decided to pursue my{" "}
-        <span className=" italic">passion for programming.</span>
+        <span>Fullstack Developer</span> with a passion for building <span className="italic">scalable, user-friendly web applications</span> using the <span className="font-bold">MERN stack</span>.<br />
+    Skilled in crafting <span className="italic">seamless front-end experiences</span> and <span className="italic">robust back-end systems</span>, with a focus on <span className="font-bold">clean code</span> and <span className="font-bold">modern technologies</span>.<br />
+    <span className="italic">Always learning</span>, <span className="italic">always building</span>, and always pushing the boundaries of what’s possible with code.
       </p>
     </motion.section>
   );
